a35dce8f6362bdfefa779528893c556a2ea3c6a3
webwalacoder <nalainahmad89@gmail.com>
2025-11-20 02:23:18 +0500

    Implement AuthForm ui with shadcn, zod, react-hook-form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
 const SignIn = () => {
-  return <div>Sign In</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 export default SignUp;
