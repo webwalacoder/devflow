2cc55784d9539a31ab857cc5d6b5d43a5532c8cb
webwalacoder <nalainahmad89@gmail.com>
2025-11-25 19:43:33 +0500

    Implement Question Card

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,30 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   js.configs.recommended,
   prettier,
+
+  // Custom override for TypeScript
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-use-before-define": "off",
+      "@typescript-eslint/no-use-before-define": ["error"],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
