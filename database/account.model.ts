15990be75b7b604ad1f1533a17bf868fb10ca446
webwalacoder <nalainahmad89@gmail.com>
2025-12-01 22:59:01 +0500

    Create Account Model

---
+++ database/account.model.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAccount {
+  userId: Types.ObjectId;
+  name: string;
+  image?: string;
+  password?: string;
+  provider: string;
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema<IAccount>({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  image: { type: String },
+  password: { type: String },
+  provider: { type: String, required: true },
+  providerAccountId: { type: String, required: true },
+});
+
+const Account = models?.Account || model<IAccount>("Account", AccountSchema);
+
+export default Account;
