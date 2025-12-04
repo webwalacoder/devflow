abb0f80ecca64193618d7ac0d9ce9cf801f6af81
webwalacoder <nalainahmad89@gmail.com>
2025-12-04 08:09:02 +0500

    Implement a Fetch Handler

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -1,6 +1,7 @@
 import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import { parseZodErrors } from "@/lib/utils";
 import { AccountSchema } from "@/lib/validations";
 import { NextResponse } from "next/server";
@@ -9,6 +10,8 @@ export async function POST(request: Request) {
   const { providerAccountId } = await request.json();
 
   try {
+    await dbConnect();
+
     const validatedData = AccountSchema.partial().safeParse({
       providerAccountId,
     });
