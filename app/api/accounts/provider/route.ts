5b0ef09b7df161761cfadfcbb42c73e98da81016
webwalacoder <nalainahmad89@gmail.com>
2025-12-04 07:05:02 +0500

    implement Account By Provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,34 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { parseZodErrors } from "@/lib/utils";
+import { AccountSchema } from "@/lib/validations";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+
+    if (!validatedData.success) {
+      const fieldErrors = parseZodErrors(validatedData.error);
+      throw new ValidationError(fieldErrors);
+    }
+
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: account,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
