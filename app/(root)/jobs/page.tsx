69b86018de12b1efa062a1393fea72ac458b657a
webwalacoder <nalainahmad89@gmail.com>
2026-02-11 16:10:44 +0500

    Implement Job Page

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -1,4 +1,55 @@
-const FindJobs = () => {
-  return <div>FindJobs</div>;
+import JobCard from "@/components/cards/JobCard";
+import JobFilter from "@/components/filters/JobFilter";
+import Pagination from "@/components/Pagination";
+import {
+  fetchCountries,
+  fetchJobs,
+  fetchLocation,
+} from "@/lib/actions/job.action";
+
+const Page = async ({ searchParams }: RouteParams) => {
+  const { query, location, page } = await searchParams;
+  const userLocation = await fetchLocation();
+  const parts = [query, location].filter(Boolean);
+
+  const searchQuery =
+    parts.length > 0
+      ? parts.join(", ")
+      : `Software Engineer in ${userLocation}`;
+
+  const jobs = await fetchJobs({
+    query: searchQuery,
+    page: page ?? 1,
+  });
+
+  const countries = await fetchCountries();
+  const parsedPage = parseInt(page ?? 1);
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Jobs</h1>
+
+      <div className="flex">
+        <JobFilter countriesList={countries} />
+      </div>
+
+      <section className="light-border mb-9 mt-11 flex flex-col gap-9 border-b pb-9">
+        {jobs.length > 0 ? (
+          jobs
+            ?.filter((job: Job) => job.job_title)
+            .map((job: Job) => <JobCard key={job.job_id} job={job} />)
+        ) : (
+          <div className="paragraph-regular text-dark200_light800 w-full text-center">
+            Oops! We couldn&apos;t find any jobs at the moment. Please try again
+            later
+          </div>
+        )}
+      </section>
+
+      {jobs?.length > 0 && (
+        <Pagination page={parsedPage} isNext={jobs?.length === 10} />
+      )}
+    </>
+  );
 };
-export default FindJobs;
+export default Page;
--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "avatars.githubusercontent.com",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
         port: "",
       },
     ],
