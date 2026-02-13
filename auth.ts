1ad6160b3ce3f35bcb07c4e130b725e80a0a6a6f
webwalacoder <nalainahmad89@gmail.com>
2026-02-13 19:21:42 +0500

    Fix: import issue for google provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { api } from "./lib/api";
 import { IAccountDoc } from "./database/account.model";
 import { SignInSchema } from "./lib/validations";
